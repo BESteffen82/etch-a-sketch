cbcfbacb7b9b47586fe2e290dddd702cbd1684b3
Brent Steffen <BESteffen82@gmail.com>
2022-04-25 13:31:39 -0400

    Add dynamic Grid properties to JS

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const container = document.querySelector('.container');
 
-let rows = 16;
-let columns = 16;
+let num = 50;
 
-for (let x=0; x< (rows * columns); x++){
+for (let x=0; x< (num * num); x++){
   const square = document.createElement('div');
   square.classList.add('square');
-  container.appendChild(square);  
-
+  container.appendChild(square);
+  
   square.addEventListener('mouseenter', e => {
-      e.target.style.backgroundColor = 'skyblue';
+    e.target.style.backgroundColor = 'skyblue';
   });
 
 }
+
+container.style.setProperty('grid-template-columns', 'repeat(' + num + ', 1fr)');
+container.style.setProperty('grid-template-rows', 'repeat(' + num + ', 1fr)'); 
