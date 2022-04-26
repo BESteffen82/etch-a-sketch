dcf17f95899d2bdb1d0e95ec5fbb574825b8460d
Brent Steffen <BESteffen82@gmail.com>
2022-04-26 14:42:29 -0400

    Add grid size and color options

--- script.js
+++ script.js
@@ -1,17 +1,62 @@
-const container = document.querySelector('.container');
 
-let num = 50;
 
-for (let x=0; x< (num * num); x++){
+function createGame(num=10){
+for (let x=0; x < (num * num); x++){
+  const container = document.querySelector('.container');
   const square = document.createElement('div');
   square.classList.add('square');
+  container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${num} , 1fr)`;
   container.appendChild(square);
   
-  square.addEventListener('mouseenter', e => {
-    e.target.style.backgroundColor = 'skyblue';
+  const black = document.querySelector('.black');
+  black.addEventListener('click', function() {
+    square.addEventListener('mouseenter', e => {
+      e.target.style.backgroundColor = 'black'
+    })
   });
+    
+  const rainbow = document.querySelector('.rainbow');
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  rainbow.addEventListener('click', function() {
+    square.addEventListener('mouseenter', e => {
+    e.target.style.backgroundColor = 'rgb('+ r +','+ g +', '+ b +')' 
+    })
+  }); 
+    
+  const clear = document.querySelector('.clear');
+  clear.addEventListener('click', function() {
+    square.style.backgroundColor = 'white'
+  });
+
+};
+}
+
+function clearGame() {
+  const container = document.querySelector('.container');
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 
 }
 
-container.style.setProperty('grid-template-columns', 'repeat(' + num + ', 1fr)');
-container.style.setProperty('grid-template-rows', 'repeat(' + num + ', 1fr)'); 
+
+let gridSlider = document.getElementById('gridRange');
+let output = document.getElementById('size');
+gridSlider.oninput = function () {
+  output.innerHTML = `${this.value} X ${this.value}`;
+  clearGame();
+  createGame(num = this.value);
+}
+
+
+
+
+
+
+
+
+
+
